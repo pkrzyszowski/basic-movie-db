a9dad9cb62cf67f7fb4241021a4d19ad2a41e3ee
Pit <szisz90@gmail.com>
2018-07-19 21:04:17 +0200

    whitenoise static

--- basic_movie_db/settings.py
+++ basic_movie_db/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 # Static/Media
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = root('media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = root('static')
